517607af5bec7916972b8d2e1ac27388358ef2a4
Marie Blanchard <134803822+mariblnd@users.noreply.github.com>
2025-06-24 09:53:24 +0200

    Update stage done

--- app/routes/stages.py
+++ app/routes/stages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 from app.models import Stage, Etudiant, Entreprise
 from app import db
 
@@ -21,7 +21,7 @@ def add_stage():
         id_etudiant = request.form.get('id_etudiant')
         id_entreprise = request.form.get('id_entreprise')
 
-        # Création d'un nouvel objet Stage
+        # Création d'un nouvel objet Stage = Permet d'ajouter une ligne dans la table Stage
         new_stage = Stage(
             domaine=domaine,
             duree=duree,
@@ -37,3 +37,28 @@ def add_stage():
     entreprises = Entreprise.query.all()
     return render_template('add_stage.html', etudiants=etudiants, entreprises=entreprises)
 
+
+@stage_bp.route('/update_stage/<int:stage_id>', methods=['GET', 'POST'])
+def update_stage(stage_id):
+
+    # Récupérer le stage à modifier
+    stage = Stage.query.get_or_404(stage_id)
+
+    # Récupérer la liste des étudiants / entreprises
+    etudiants = Etudiant.query.all()
+    entreprises = Entreprise.query.all()
+
+    if request.method == 'POST':
+        stage.domaine = request.form['domaine']
+        stage.duree = request.form['duree']
+        stage.id_etudiant = request.form['id_etudiant']
+        stage.id_entreprise = request.form['id_entreprise']
+        db.session.commit()
+        return redirect(url_for('stage_bp.liste_stages'))  # index.html
+
+    return render_template(
+        'update_stage.html',
+        stage=stage,
+        etudiants=etudiants,
+        entreprises=entreprises
+    )
